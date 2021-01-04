0293ad487aa94ccb8e27ed7a53e1f806424f9e57
JC <joepaulcollins@gmail.com>
2021-01-04 14:58:51 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loggedInUser: null,
+  };
+
+  login = (user) => {
+    this.setState({ loggedInUser: user });
+  };
+
+  logout = () => {
+    this.setState({ loggedInUser: null });
+  };
+
+  render() {
+    const { loggedInUser } = this.state;
+    return (
+      <UserContext.Provider
+        value={{ loggedInUser, login: this.login, logout: this.logout }}
+      >
+        <div className="App">
+          <Nav-bar />
+          <Router>
+            <ErrorHandling
+              default
+              errorMsg="This isn't the page you're looking for..."
+            />
+          </Router>
+        </div>
+      </UserContext.Provider>
+    );
+  }
 }
 
 export default App;
