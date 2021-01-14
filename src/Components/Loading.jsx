77459027a68ba255eef2a8ffd28c5a0bece388ca
Clara Boothby <clarahboothby@gmail.com>
2021-01-14 08:44:33 +0000

    adding styles to loading component

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,4 +1,15 @@
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    color: theme.palette.primary.main,
+    padding: "1.5rem",
+  },
+}));
+
 export default function Loading() {
-  return <CircularProgress />;
+  const classes = useStyles();
+
+  return <CircularProgress className={classes.loading} />;
 }
